121a9e920211c3f6f38d0801c7628baf92e0baf3
NickBrody <deliciousman99@gmail.com>
2024-06-10 19:43:34 +0900

    3step

--- database.py
+++ database.py
@@ -1,19 +1,19 @@
 from typing import Dict, Any
 
-from sqlalchemy import Column, Integer,create_engine
+from sqlalchemy import Column, Integer, create_engine, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-engine = create_engine("postgresql+psycopg2://admin:admin@db:5432/test_db")
+engine = create_engine("postgresql+psycopg2://admin:123@localhost:5432/documents")
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
 
 class Document(Base):
-    __tablename__ = 'documents'
+    __tablename__ = 'snils_data'
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    number = Column(Integer, nullable=False)
+    snils = Column(String, nullable=False)
 
     def to_json(self) -> Dict[str, Any]:
         return {c.name: getattr(self, c.name) for c in
